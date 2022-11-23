169c25c5c1358ab4dfafa7de6cdbf48747d37351
emonShimoul <emonshimoul@gmail.com>
2022-11-24 00:50:55 +0600

    get todoInfo from server

--- index.js
+++ index.js
@@ -13,12 +13,17 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pabg0.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-
-
 async function run() {
     try {
         await client.connect();
         console.log('database connected successfully!!');
+        const database = client.db('todoapp');
+        const todoInfoCollection = database.collection('todoinfo');
+
+        app.post('/todoInfo', async (req, res) => {
+            const books = req.body;
+            console.log(books);
+        })
     }
     finally {
         // await client.close(); 
